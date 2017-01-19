8eaed931b574890c72cd915f4b8ba65806d3b1d8
Caitlin Mueller <caitlinm@mit.edu>
2017-01-19 13:16:26 -0500

    Added Math.NET library

---
+++ Libraries/MathNet.Numerics/src/Numerics/Distributions/ISetDistribution.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MathNet.Numerics.Distributions
+{
+    public interface ISetDistribution : IContinuousDistribution
+    {
+        new double Mean {get; set;}
+        new double StdDev { get; set; }
+    }
+}
